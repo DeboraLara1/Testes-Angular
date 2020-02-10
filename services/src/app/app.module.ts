2ef99e5473f861515f1e7330912a19a718aaad2b
debora.roldanlara@gmail.com <debora.roldanlara@gmail.com>
2020-02-09 22:07:37 -0300

    Injeção de Dependencias (DI) + como usar um serviço em um componente

--- services/src/app/app.module.ts
+++ services/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { CursosService } from './../../../primeiro-Projeto/src/app/meu-primeiro/cursos/cursos.service';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,7 @@ import { CursosComponent } from './cursos/cursos.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
