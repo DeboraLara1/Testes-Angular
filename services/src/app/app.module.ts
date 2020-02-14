ce70c8bc5f234bfd443d1005b969428940107b1f
debora.roldanlara@gmail.com <debora.roldanlara@gmail.com>
2020-02-14 18:36:54 -0300

    Injetando um serviço em outro serviço

--- services/src/app/app.module.ts
+++ services/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { CursoModule } from './cursos/cursos.module';
 import { CursosService } from './cursos/cursos.service';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
+import { LogService } from './shared/log.service';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { CriarCursoModule } from './criar-curso/criar-curso.module';
     CursoModule
 
   ],
-  providers: [CursosService],
+  providers: [LogService],
+  //providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
