5cf0a101ce30b72b3b7f7234bc75867fa2a0726e
debora.roldanlara@gmail.com <debora.roldanlara@gmail.com>
2020-02-17 14:55:47 -0300

    Criando um Pipe

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -4,11 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
+import { CamelPipePipe } from './exemplos-pipes/camel-pipe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExemplosPipesComponent
+    ExemplosPipesComponent,
+    CamelPipePipe
+
   ],
   imports: [
     BrowserModule,
---
+++ pipes/src/app/exemplos-pipes/camel-pipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CamelPipePipe } from './camel-pipe.pipe';
+
+describe('CamelPipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new CamelPipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
