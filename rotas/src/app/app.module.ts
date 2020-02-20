c0e3d026d7bc0416cf830e1f5607fedb10a1ece3
debora.roldanlara@gmail.com <debora.roldanlara@gmail.com>
2020-02-19 23:41:57 -0300

    Subindo mosificações e configurações atualizadas de rotas

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -3,18 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MaterializeModule } from 'angular2-materialize';
 
 @NgModule({
   declarations: [
     AppComponent
+
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    MaterializeModule
+    AppRoutingModule
+
   ],
+
   providers: [],
+
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
